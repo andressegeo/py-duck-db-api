21b6dd82a5fb2718df3c3dbd1c737a96484ebc8a
Kévin LAMBERT <kevin.lambert@free.fr>
2016-10-23 19:37:34 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='db-api',    # This is the name of your PyPI-package.
+    version='0.1',                          # Update the version number for new releases
+    packages=['db_api']
+)
